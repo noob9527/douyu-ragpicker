17c4f0a5db59df933231242578223b09109b384c
noob9527 <2061864@qq.com>
2018-12-12 03:31:29 +0800

    bg script add listener

--- src/app/event-page/event-page.component.ts
+++ src/app/event-page/event-page.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 
+
 @Component({
   selector: 'app-event-page',
   templateUrl: './event-page.component.html',
@@ -7,9 +8,32 @@ import { Component, OnInit } from '@angular/core';
 })
 export class EventPageComponent implements OnInit {
 
-  constructor() { }
+  constructor() {
+  }
 
   ngOnInit() {
   }
 
 }
+
+if (chrome && chrome.runtime && chrome.runtime.onInstalled) {
+  const DOUYU_DOMAIN = 'douyu.com';
+
+  chrome
+    .runtime
+    .onInstalled
+    .addListener(function () {
+      chrome.declarativeContent.onPageChanged
+        .removeRules(undefined, function () {
+          chrome.declarativeContent.onPageChanged
+            .addRules([{
+              conditions: [new chrome.declarativeContent.PageStateMatcher({
+                pageUrl: { hostContains: DOUYU_DOMAIN },
+              })
+              ],
+              actions: [new chrome.declarativeContent.ShowPageAction()]
+            }]);
+        });
+    });
+}
+
